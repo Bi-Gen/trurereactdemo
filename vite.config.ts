7741847cc8a8b4a41957722e8b97892f601f8292
Michele Sciabarra <msciabarra@apache.org>
2025-11-30 11:20:51 +0000

    trustable workspace

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  build: {
-    outDir: "web"
-  },
-  server: {
-    proxy: {
-      "/api/my": {
-        target: process.env.VITE_OPSDEV_HOST || process.env.OPS_HOST,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    build: {
+      outDir: "web"
+    },
+    server: {
+      proxy: {
+        "/api/my": {
+          target: env.OPS_HOST || env.OPSDEV_HOST,
         changeOrigin: true
       }
     },
     host: "0.0.0.0",
     port: 5173,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+};
+});
